e4794aefc39bc9fcd5b577ffcee2a609347f1f30
Tudor Avram <a96tudor@gmail.com>
2017-04-23 05:37:30 +0200

    finished insert database handlers

--- server/server.py
+++ server/server.py
@@ -1,14 +1,51 @@
-from flask import Flask
+from flask import Flask, request, jsonify, json, Response
 import urllib
 import ssl
+from DatabaseHandler import DatabaseHandler as Dh
+import hashlib
+from passlib.hash import pbkdf2_sha256
 
 _API_KEY = "JTNTukkrHomshtW9HthnNtHgevJop1SkQ2FjsnY2JgDdYDoS77"
 
 app = Flask(__name__)
 
+dh = Dh("food-bot")
+
+@app.route("/signup1", methods=["POST"])
+def signup1():
+    if request.is_json:
+        data = request.json
+        if len(data) == 3 and \
+            'email' in data and \
+            'password' in data and \
+            'name' in data:
+
+            uID, success = dh.signup_step_one(data['email'], data['password'], data['name'])
+
+
+    else:
+        #error
+        return Response(response="Expected JSON", status=400)
+
 @app.route("/login", methods=["POST"])
 def login():
-    
+    if request.is_json:
+        data = request.json
+        if len(data) == 2 and 'email' in data and 'password' in data:
+            success, uID = dh.try_to_login_user(
+                data['email'],
+                data['password']
+            )
+
+            result = dict()
+            result['success'] = success
+            result['userID'] = uID
+
+            return jsonify(result)
+
+    else:
+        #error
+        return Response(response="Expected JSON", status=400)
 
 @app.route("/get-todays-menu")
 def get_todays_menu():
