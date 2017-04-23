506669f2aef5c6d6f8669442a97db124c3d36b23
Lucia Bura <luciabura@gmail.com>
2017-04-23 08:39:04 +0100

    Updates to server.py function to get recipe.

--- server/server.py
+++ server/server.py
@@ -4,8 +4,7 @@ import ssl
 from DatabaseHandler import DatabaseHandler as Dh
 import hashlib
 from passlib.hash import pbkdf2_sha256
-import urllib.parse as urlparse
-from urllib.parse import urlencode1
+import datetime
 
 _API_KEY = "JTNTukkrHomshtW9HthnNtHgevJop1SkQ2FjsnY2JgDdYDoS77"
 
@@ -51,8 +50,97 @@ def login():
         #error
         return Response(response="Expected JSON", status=400)
 
-def modify_URL():
+@app.route("/get-recipe",methods=['POST'])
+def get_recipe():
+    context = ssl._create_unverified_context()
 
+    if request.is_json:
+        data = request.json
+        if len(data) == 2 and 'keyword' in data:
+            keyword = data['keyword']
+
+    intolerances = dh.get_intolerances()
+    diet = dh.get_preferences()
+    targetCalories = dh.get_kcal()
+    limitNumber = 5
+
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?"
+
+    for i, val in enumerate(diet):
+        if (i == 0):
+            url += "diet="
+        else:
+            if (i != 0):
+                url += "%2C+"
+            url += val
+
+    for i, val in enumerate(intolerances):
+        if (i == 0):
+            url += "&"
+            url += "intolerances="
+        else:
+            if (i != 0):
+                url += "%2C+"
+            url += val
+
+    if(targetCalories):
+        url += "&maxCalories=" + targetCalories
+
+    url += "&number=" + limitNumber
+
+    if(keyword):
+        url += "&query=" + keyword
+
+
+    req = urllib.request.Request(url)
+    req.add_header("X-Mashape-Key", _API_KEY)
+    req.add_header("Accept", "application/json")
+
+    data = urllib.request.urlopen(req, context=context).read()
+
+    return data.decode()
+
+@app.route("/get-week-menu",methods=['POST'])
+def get_week_menu():
+    context = ssl._create_unverified_context()
+
+    intolerances = dh.get_intolerances()
+    diet = dh.get_preferences()
+    targetCalories = dh.get_kcal()
+    timeFrame = 'week'
+
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/mealplans/generate?"
+
+    for i, val in enumerate(diet):
+        if (i == 0):
+            url += "diet="
+        else:
+            if (i != 0):
+                url += "%2C+"
+            url += val
+
+    for i, val in enumerate(intolerances):
+        if (i == 0):
+            url += "&"
+            url += "exclude="
+        else:
+            if (i != 0):
+                url += "%2C+"
+            url += val
+
+    if(targetCalories):
+        url += "&targetCalories" + targetCalories
+
+    url += "&timeFrame=" + timeFrame
+
+
+    req = urllib.request.Request(url)
+    req.add_header("X-Mashape-Key", _API_KEY)
+    req.add_header("Accept", "application/json")
+
+    data = urllib.request.urlopen(req, context=context).read()
+
+    return data.decode()
 
 @app.route("/get-todays-menu",methods=['POST'])
 def get_todays_menu():
@@ -64,10 +152,30 @@ def get_todays_menu():
     timeFrame = 'day'
 
     url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/mealplans/generate?"
+
     for i, val in enumerate(intolerances):
-        if i = 0:
-            url += "intolerance="
+        if (i == 0):
+            url += "&"
+            url += "exclude="
         else:
+            if (i != 0):
+                url += "%2C+"
+            url += val
+
+    for i, val in enumerate(diet):
+        if (i == 0):
+            url += "&"
+            url += "diet="
+        else:
+            if (i != 0):
+                url += "%2C+"
+            url += val
+
+    if(targetCalories):
+        url += "&" + targetCalories
+
+    url += "&" + timeFrame
+
 
     req = urllib.request.Request(url)
     req.add_header("X-Mashape-Key", _API_KEY)
